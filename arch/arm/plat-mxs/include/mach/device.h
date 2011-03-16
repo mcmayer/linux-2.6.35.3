66dda0dfa47622348484072bc0ac70c1e7643b54
Tony Lin <tony.lin@freescale.com>
2011-03-16 10:43:26 +0800

    ENGR00140699-2 [mx28 spi] add a parameter to select spi master/slave mode
    add a parameter to select spi master/slave mode
    by default, spi master mode is selected.
    
    Signed-off-by: Tony Lin <tony.lin@freescale.com>

--- arch/arm/plat-mxs/include/mach/device.h
+++ arch/arm/plat-mxs/include/mach/device.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009-2010 Freescale Semiconductor, Inc. All Rights Reserved.
+ * Copyright (C) 2009-2011 Freescale Semiconductor, Inc. All Rights Reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -123,6 +123,7 @@ struct mxs_spi_platform_data {
 	int (*hw_pin_init)(void);
 	int (*hw_pin_release)(void);
 
+	int slave_mode;
 	char *clk;
 };
 
